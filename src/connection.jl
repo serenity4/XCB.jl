82a397cb3bbafeb3712615a41151f46202e8b0cf
serenity4 <cedric.bel@hotmail.fr>
2020-11-21 17:55:57 +0100

    Re-arrange code

--- src/connection.jl
+++ src/connection.jl
@@ -54,28 +54,6 @@ end
     XCB_CONN_CLOSED_FDPASSING_FAILED = 7
 end
 
-
-function Base.show(io::IO, setup::xcb_setup_t)
-    desc = ["Version"]
-    vals = [VersionNumber(setup.protocol_major_version, setup.protocol_minor_version, setup.release_number)]
-    println(io, "Setup")
-    prettyprint(io, desc, vals)
-end
-
-function format_bignumber(number)
-    number_str = string(number)
-    join(map((i, x) -> (mod(i, 3) == 0 && i ≠ length(number_str) ? "," : "") * x, length(number_str):-1:1, number_str))
-end
-
-prettyprint(io, desc, vals) = (description = join.(zip(rpad.(desc, 20), vals), ": "); print(io, join("    " .* description, "\n")))
-
-function Base.show(io::IO, screen::xcb_screen_t)
-    desc = ["White pixel value", "Black pixel value", "Width", "Height"]
-    vals = Any[format_bignumber(screen.white_pixel), format_bignumber(screen.black_pixel), "$(screen.width_in_pixels) pixels, $(screen.width_in_millimeters / 10) cm", "$(screen.height_in_pixels) pixels, $(screen.height_in_millimeters / 10) cm"]
-    println(io, "Screen details")
-    prettyprint(io, desc, vals)
-end
-
 """
 Check that the connection to the X server was successful. Throws a [`ConnectionError`](@ref) if the connection failed.
 """
@@ -99,7 +77,6 @@ function check_flush(code)
     end
 end
 
-Base.showerror(io::IO, error::FlushError) = print("FlushError: server returned code $(error.code)")
 Base.flush(connection::Connection) = check_flush(xcb_flush(connection))
 
 """
@@ -179,3 +156,26 @@ macro check(request)
     end
     :($(esc(check_request_fun))($(esc(conn)), $(esc(request))))
 end
+
+Base.showerror(io::IO, error::FlushError) = print("FlushError: server returned code $(error.code)")
+
+function Base.show(io::IO, setup::xcb_setup_t)
+    desc = ["Version"]
+    vals = [VersionNumber(setup.protocol_major_version, setup.protocol_minor_version, setup.release_number)]
+    println(io, "Setup")
+    prettyprint(io, desc, vals)
+end
+
+function Base.show(io::IO, screen::xcb_screen_t)
+    desc = ["White pixel value", "Black pixel value", "Width", "Height"]
+    vals = Any[format_bignumber(screen.white_pixel), format_bignumber(screen.black_pixel), "$(screen.width_in_pixels) pixels, $(screen.width_in_millimeters / 10) cm", "$(screen.height_in_pixels) pixels, $(screen.height_in_millimeters / 10) cm"]
+    println(io, "Screen details")
+    prettyprint(io, desc, vals)
+end
+
+function format_bignumber(number)
+    number_str = string(number)
+    join(map((i, x) -> (mod(i, 3) == 0 && i ≠ length(number_str) ? "," : "") * x, length(number_str):-1:1, number_str))
+end
+
+prettyprint(io, desc, vals) = (description = join.(zip(rpad.(desc, 20), vals), ": "); print(io, join("    " .* description, "\n")))
